31c5c409617cdb626c61f488f3b072ddde0ba5ee
yashthetiger <64192060+yashthetiger@users.noreply.github.com>
2020-07-07 10:38:40 +0530

    create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,30 @@
+//1.program to print size of all data types(char,int,float,double)
+#include<iostream>
+using namespace std;
+int main()
+{
+	char a;
+	int b;
+	float c;
+	double d;
+	//sizeof(variable) used for finding size of a datatype
+	//endl command moves the cursor to next line
+	cout<<"Size of char:"<<sizeof(a)<<" byte"<<endl;
+	cout<<"Size of int:"<<sizeof(b)<<" bytes"<<endl;
+	cout<<"Size of float:"<<sizeof(c)<<" bytes"<<endl;
+	cout<<"Size of double:"<<sizeof(d)<<" bytes"<<endl;
+	return 0;
+}
+
+//2.program to print the product of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,product;
+	cout<<"Enter two numbers:";
+	cin>>a>>b;
+	product=a*b;
+	cout<<"Product = "<<product<<endl;
+	return 0;
+}
